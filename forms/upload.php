af746bfcd6f8e7ebec0490a0812d21a26b79130f
Josearratia <1630037@upv.edu.mx>
2020-04-03 19:09:22 +0000

    add photo

--- forms/upload.php
+++ forms/upload.php
@@ -4,15 +4,39 @@ include_once 'user.php';
 include_once 'Session.php';
 
 $userSession = new UserSession();
-/* $user = new login(); */
+$user = new login(); 
 
-/* if (isset($_SESSION['user'])) {
+ if (isset($_SESSION['user'])) {
     $user->setUserAndfk($userSession->getCurrentUser());
     $directorio = "assets/upload/img/uploadimg/";
 
-    $archivo = $directorio . basename($user->getusercode());
-    echo $archivo;
+    $archivo = $directorio . basename($_FILES['mp']["name"]);
+    
+    $tipoArchivo = strtolower(pathinfo($archivo, PATHINFO_EXTENSION));
+
+    $isimg = getimagesize($_FILES["mp"]["tmp_name"]);
+
+    if($isimg != false){
+        $size = $_FILES["mp"]["size"];
+
+        if($size > 10000000){
+            echo "Archivo mayor a 10mb";
+        }else {
+            if($tipoArchivo == "jpg"){
+                if(move_uploaded_file($_FILES["mp"]["tmp_name"],$archivo)){
+                    echo "Archivo guardado";
+                }else{
+                    echo "error al guardar el archivo";
+                }
+            }else{
+                echo "solo se permitente archivos jpg";
+            }
+        }
+
+    }else {
+        echo "Archivo no valido";
+    }
 } else {
     include_once 'index.php';
-} */
+} 
     
